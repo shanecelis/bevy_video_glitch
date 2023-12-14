0862a9800549f75b621f238c86ba281d027fa5e5
Shane Celis <shane.celis@gmail.com>
2023-12-14 05:42:21 -0500

    hack: Start of something. Not working.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,176 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+use bevy::{
+    prelude::*,
+    reflect::{TypePath, TypeUuid},
+    asset::load_internal_asset,
+    core_pipeline::clear_color::ClearColorConfig,
+    render::{
+        camera::RenderTarget,
+        render_resource::{
+            AsBindGroup, Extent3d, ShaderRef, TextureDescriptor, TextureDimension, TextureFormat,
+            TextureUsages,
+        },
+        texture::BevyDefault,
+        view::RenderLayers,
+    },
+    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
+};
+pub const VIDEO_GLITCH_SHADER_HANDLE: Handle<Shader> = Handle::weak_from_u128(0x7b1d58197dc34e26b0c69a3c8091a014u128);
+
+#[derive(Default)]
+pub struct VideoGlitchPlugin;
+
+impl Plugin for VideoGlitchPlugin {
+    fn build(&self, app: &mut App) {
+        load_internal_asset!(app, VIDEO_GLITCH_SHADER_HANDLE, "video-glitch.wgsl", Shader::from_wgsl);
+
+        app.add_plugins(Material2dPlugin::<VideoGlitchMaterial>::default())
+            // .add_systems(Update, update_mtoon_shader);
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    windows: Query<&Window>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut post_processing_materials: ResMut<Assets<VideoGlitchMaterial>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+) {
+    // This assumes we only have a single window
+    let window = windows.single();
+
+    let size = Extent3d {
+        width: window.resolution.physical_width(),
+        height: window.resolution.physical_height(),
+        ..default()
+    };
+
+    // This is the texture that will be rendered to.
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::bevy_default(),
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_DST
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
+
+    // fill image.data with zeroes
+    image.resize(size);
+
+    let image_handle = images.add(image);
+
+    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 4.0 }));
+    let cube_material_handle = materials.add(StandardMaterial {
+        base_color: Color::rgb(0.8, 0.7, 0.6),
+        reflectance: 0.02,
+        unlit: false,
+        ..default()
+    });
+
+    // The cube that will be rendered to the texture.
+    commands.spawn((
+        PbrBundle {
+            mesh: cube_handle,
+            material: cube_material_handle,
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0)),
+            ..default()
+        },
+        MainCube,
+    ));
+
+    // Light
+    // NOTE: Currently lights are ignoring render layers - see https://github.com/bevyengine/bevy/issues/3462
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 10.0)),
+        ..default()
+    });
+
+    // Main camera, first to render
+    commands.spawn((
+        Camera3dBundle {
+            camera_3d: Camera3d {
+                clear_color: ClearColorConfig::Custom(Color::WHITE),
+                ..default()
+            },
+            camera: Camera {
+                target: RenderTarget::Image(image_handle.clone()),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 15.0))
+                .looking_at(Vec3::default(), Vec3::Y),
+            ..default()
+        },
+        // Disable UI rendering for the first pass camera. This prevents double rendering of UI at
+        // the cost of rendering the UI without any post processing effects.
+        UiCameraConfig { show_ui: false },
+    ));
+
+    // This specifies the layer used for the post processing camera, which will be attached to the post processing camera and 2d quad.
+    let post_processing_pass_layer = RenderLayers::layer((RenderLayers::TOTAL_LAYERS - 1) as u8);
+
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
+        size.width as f32,
+        size.height as f32,
+    ))));
+
+    // This material has the texture that has been rendered.
+    let material_handle = post_processing_materials.add(VideoGlitchMaterial {
+        source_image: image_handle,
+    });
+
+    // Post processing 2d quad, with material using the render texture done by the main camera, with a custom shader.
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: quad_handle.into(),
+            material: material_handle,
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 1.5),
+                ..default()
+            },
+            ..default()
+        },
+        post_processing_pass_layer,
+    ));
+
+    // The post-processing pass camera.
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                // renders after the first main camera which has default value: 0.
+                order: 1,
+                ..default()
+            },
+            ..Camera2dBundle::default()
+        },
+        post_processing_pass_layer,
+    ));
+}
+// Region below declares of the custom material handling post processing effect
+
+/// Our custom post processing material
+#[derive(AsBindGroup, TypeUuid, Clone)]
+#[uuid = "fdf93f23-102c-4e9e-8df7-09fddff355a9"]
+struct VideoGlitchMaterial {
+    /// In this example, this image will be the result of the main camera.
+    #[texture(0)]
+    #[sampler(1)]
+    source_image: Handle<Image>,
+}
+
+impl Material2d for VideoGlitchMaterial {
+    fn fragment_shader() -> ShaderRef {
+        // "shaders/custom_material_chromatic_aberration.wgsl".into()
+        "shaders/video-glitch.wgsl".into()
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +179,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        // assert_eq!(result, 4);
     }
 }
